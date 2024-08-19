083ffc0002abcf578f8f56fb87ebb972e57fdbd1
Caio Santos <caiohenriquedassantos@gmail.com>
2024-08-19 11:07:13 -0300

    feat: add handler to list items in groups of 4

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,48 @@
 import { useEffect, useState } from "react"
 import { fetchApi } from "../services/fetchApi"
+import "./Home.css"
 
 function Home() {
 
   const [data, setData] = useState([])
+  const [moreItems, setMoreItems] = useState(4)
 
   useEffect(() => {
     const getApi = async () => {
       const res = await fetchApi()
       setData(res.results)
+      console.log(res)
     }
     getApi()
   }, [])
 
+  const handleClickMore = () => {
+    setMoreItems(moreItems + 4)
+  }
+
   return (
     <>
-      <div>
-        {data.map((e, index) => (
-          <div>
-            <h1>{e.title}</h1>
-          </div>
-        ))}
+      <header>
+        <div className="topnav">
+          <input type="text" placeholder="Buscar" />
+          <i class="bi bi-heart heart-icon"></i>
+        </div>
+      </header>
+      <div className="container">
+        <h1 className="title">Livros</h1>
+        <div>
+          {data.slice(0, moreItems).map((e, index) => (
+            <div className="book-card">
+              <img src={e.thumbnail} className="book-image-placeholder" />
+              <div className="book-info">
+                <h2>{e.title}</h2>
+                <p>{e.description}</p>
+                <a href="#">Saiba Mais</a>
+              </div>
+            </div>
+          ))}
+          <button onClick={handleClickMore}>Ver Mais</button>
+        </div>
       </div>
     </>
   )
